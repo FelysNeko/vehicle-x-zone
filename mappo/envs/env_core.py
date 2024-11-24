2ac59c9edfea233c2f97b8bd095fcf43e633277b
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2024-11-25 01:06:42 +0800

    update

---
+++ mappo/envs/env_core.py
@@ -0,0 +1,31 @@
+from environ.components import Zone
+from environ.scenario import Scenario
+
+
+class EnvCore(object):
+    def __init__(self):
+        self.scenario = Scenario()
+        self.agent_num = 7
+        self.obs_dim = 76
+        self.action_dim = 6
+
+    def reset(self):
+        sub_agent_obs = []
+        for obs in self.scenario.reset():
+            sub_agent_obs.append(obs)
+        return sub_agent_obs
+
+    def step(self, actions):
+        sub_agent_obs = []
+        sub_agent_reward = []
+        sub_agent_done = []
+        sub_agent_info = []
+
+        zones = (Zone(i[:2], i[2:4], i[4:6]) for i in actions)
+        for obs, reward, done in self.scenario.step(zones):
+            sub_agent_obs.append(obs)
+            sub_agent_reward.append([reward])
+            sub_agent_done.append(done)
+            sub_agent_info.append({})
+
+        return [sub_agent_obs, sub_agent_reward, sub_agent_done, sub_agent_info]
